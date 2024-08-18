9ab3a6d838659ee142bcf0a8e0e035cd47ab9e6e
heodongun <heodongun0922@gmail.com>
2024-08-19 03:14:04 +0900

    login완료

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ app.use(cors());
 const port = 3000;
 require('dotenv').config();
 
+app.use(session({
+  secret: '암호화에 쓸 비번',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
+}));
 
 let db;
 const url = mongoURI;
 
 new MongoClient(url).connect().then((client) => {
   console.log('DB연결성공');
-  db = client.db('forum');
+  db = client.db('allergic');
 }).catch((err) => {
   console.log(err);
 });
@@ -27,9 +33,24 @@ app.listen(port, () => {
 
 
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { title: 'EJS with Node.js' });
-});
-
-
-
+app.get('/login', async (req, res) => {
+  const userId = req.query.userid;
+  const userPs = req.query.userpassword;
+  let user= await db.collection('user').findOne({ userId:userId});
+  console.log(userId)
+  console.log(user.userId)
+  console.log(userPs)
+  console.log(user.userPs)
+  if(!user){
+    res.json({message:"아이디없음"})
+  }
+  else{
+    if(user.userPs==userPs){
+      res.json({userId:userId,userPs:userPs})
+    }
+    else{
+      res.json({message:"비밀번호 틀림"})
+    }
+  }
+  })
+  
